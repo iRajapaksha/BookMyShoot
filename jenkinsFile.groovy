a6928558131a504037574cbbb2807bfd4bd40bb0
iRajapaksha <nayanajith.ishara2541@gmail.com>
2024-06-28 13:47:35 +0530

    pipeline update

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -8,6 +8,9 @@ triggers{
         BRANCH = 'main'
         DOCKER_REGISTRY = 'irajapaksha'
         APP_NAME = 'BookMyShoot'
+        EC2_USER = 'ubuntu'
+        EC2_HOST = '172.31.45.108'
+
     }
 
     stages {
@@ -41,14 +44,20 @@ triggers{
             }
         }
 
-        stage('Deploy Application') {
-            steps {
-                script {
-                    sh 'docker-compose down'
-                    sh 'docker-compose up -d'
-                }
-            }
+    stage('Deploy Application on EC2') {
+        steps {
+        script {
+            sh """
+            ssh -i ${EC2_USER}@${EC2_HOST} << EOF
+            cd ~/TMS
+            docker-compose down
+            docker-compose up -d
+            exit
+            EOF
+            """
         }
+    }
+}
     }
 }
 
